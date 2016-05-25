4a0279039afc1f6200a2f1fdafe95e095f21e1b2
Jorge Gomez <gomezjorge.exequiel@gmail.com>
2016-05-24 21:10:21 -0300

    Aes 128,192,256 encrypt working with cbc.

--- src/Algorithms/AES.h
+++ src/Algorithms/AES.h
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <openssl/bio.h>
 
-int encrypt(const char* key, int AESType, const char* cin, const char* cout);
+#include "../Utilities/bitManipulation.h"
+
+#define MAX_ENCR_LENGTH 1024
+#define SUCCESS 0
+#define FAILURE !SUCCESS
+
+int encrypt(const unsigned char* key, EVP_CIPHER* cipher, const unsigned char* in, unsigned char** out, int* len);
+
+int saveEncryptedData(unsigned char *out, int len,unsigned char *where);
+
+void mostrarKey(unsigned char key[]);
