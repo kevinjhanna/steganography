643fe5986ee11ff269a729e7c417410138a7d9ab
Jorge Gomez <gomezjorge.exequiel@gmail.com>
2016-05-24 21:10:21 -0300

    Aes 128,192,256 encrypt working with cbc.

--- src/Algorithms/AES.c
+++ src/Algorithms/AES.c
@@ -1,81 +1,73 @@
 #include "AES.h"
 
-int encrypt(const char* key, int AESType, const char* cin, const char* fout) {
+//TODO
+/*
+Now out is a filepath, it should be the data
+*/
+int encrypt(const unsigned char* pwd, EVP_CIPHER* cipher, const unsigned char* in, unsigned char** out, int* len) {
+ 	unsigned char *key = malloc(sizeof(unsigned char) * EVP_CIPHER_key_length(cipher));
+	unsigned char *iv = malloc(sizeof(unsigned char) * EVP_CIPHER_iv_length(cipher));
+	int outl, templ;
 
-    AES_KEY ks;
-    unsigned char* in;
-    unsigned char* out;
-    unsigned char* cout;
-    unsigned char* inPad;
-    unsigned char* uKey;
-    int inl, outl;
+	*out = (unsigned char * ) malloc(MAX_ENCR_LENGTH * sizeof(BYTE));
+	
+	//Key Generation. We don't need to use Salt
+ 	EVP_BytesToKey(cipher, EVP_md5(), NULL, pwd, strlen(pwd),1, key, iv); 
+ 	
+	//Context initialization
+	EVP_CIPHER_CTX ctx;
+	EVP_CIPHER_CTX_init(&ctx);
 
-    if (strlen(key)*8 != AESType) {
-        printf("Key length does not match with AES-%d", AESType);
-        return -1;
-    }
-
-    toUnsignedChar(key,uKey);
-    AES_set_encrypt_key(uKey, AESType, &ks); 
-
-    toUnsignedChar(cin, in);
-    /* Padding */
-    inl = strlen(in);
-    padding(in, &inl, AES_BLOCK_SIZE, inPad); 
-
-    /* Modo ecb encripta de a BLOQUES*/
-    out = malloc (inl);
-    outl = inl;
-    EncryptECB(inPad, inl, out, &ks);
-
-    toUnsignedChar(fout, cout);
-    saveEncryptedData(out, outl, cout);
-    
-    free(in);
-    free(out);
-    return 0; 
-
+	//Encription parameters
+	EVP_EncryptInit_ex(&ctx, cipher, NULL, key, iv);
+	EVP_EncryptUpdate(&ctx, out, &outl, in, strlen(in));
+	EVP_EncryptFinal(&ctx, *out + outl, &templ);
+	
+	//Cleaning context
+	EVP_CIPHER_CTX_cleanup(&ctx);
+	*len = outl + templ;
+	return EXIT_SUCCESS;
 }
 
-int decrypt() {
-    //TODO
-    return 0;
-}
+// // int decrypt() {
+// //     //TODO
+// //     return 0;
+// // }
 
-int toUnsignedChar(const char* in, unsigned char* out) {
-    out = malloc(strlen(in) * sizeof(unsigned char));
-    strncpy(out, in, strlen(in));
-    return 0;
-}
+// // int toUnsignedChar(const char* in, unsigned char* out) {
+// //     out = malloc(strlen(in) * sizeof(unsigned char));
+// //     strncpy(out, in, strlen(in));
+// //     return 0;
+// // }
 
-void EncryptECB(unsigned char *in, int inl, unsigned char *out, AES_KEY *ks)
-{
-    unsigned char inB[AES_BLOCK_SIZE]; 
-    unsigned char outB[AES_BLOCK_SIZE];
-    int numB;
-    int i;
-    numB = inl / AES_BLOCK_SIZE;
-    for (i = 0; i < numB; i++) {
-        memcpy(inB, in + i * AES_BLOCK_SIZE, AES_BLOCK_SIZE);
-        AES_ecb_encrypt(inB, outB, ks, AES_ENCRYPT);
-        memcpy(out + i * AES_BLOCK_SIZE, outB, AES_BLOCK_SIZE);
-    }
-    return;
-}
+// // void EncryptECB(unsigned char *in, int inl, unsigned char *out, AES_KEY *ks)
+// // {
+// //     unsigned char inB[AES_BLOCK_SIZE]; 
+// //     unsigned char outB[AES_BLOCK_SIZE];
+// //     int numB;
+// //     int i;
+// //     numB = inl / AES_BLOCK_SIZE;
+// //     for (i = 0; i < numB; i++) {
+// //         memcpy(inB, in + i * AES_BLOCK_SIZE, AES_BLOCK_SIZE);
+// //         AES_ecb_encrypt(inB, outB, ks, AES_ENCRYPT);
+// //         memcpy(out + i * AES_BLOCK_SIZE, outB, AES_BLOCK_SIZE);
+// //     }
+// //     return;
+// // }
 
-void padding(unsigned char *in, int *inl, size_t blocksize, unsigned char* inPad) {
-    int pad;
-    int i;
-    pad = blocksize - (*inl) % blocksize;
-    inPad = malloc(*inl + pad);
-    memcpy(inPad, in, *inl);
-    for (i = (*inl); i < (*inl + pad); i++)
-        inPad[i] = pad;
-    *inl +=pad;
-    return;
-} 
+// // void padding(unsigned char *in, int *inl, size_t blocksize, unsigned char* inPad) {
+// //     int pad;
+// //     int i;
+// //     pad = blocksize - (*inl) % blocksize;
+// //     inPad = malloc(*inl + pad);
+// //     memcpy(inPad, in, *inl);
+// //     for (i = (*inl); i < (*inl + pad); i++)
+// //         inPad[i] = pad;
+// //     *inl +=pad;
+// //     return;
+// // } 
 
-int saveEncryptedData(unsigned char *out, int len,unsigned char *where) {
+int saveEncryptedData(unsigned char *out, int len, unsigned char *where) {
     BIO *b64;
     BIO *bio;
     b64 = BIO_new(BIO_f_base64());
