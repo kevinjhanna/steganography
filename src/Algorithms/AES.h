292968f57a4a3c95af1699d34bebcd5dc9a7c09c
Jorge Gomez <gomezjorge.exequiel@gmail.com>
2016-06-20 14:02:11 -0300

    Fixed merge issues.

--- src/Algorithms/AES.h
+++ src/Algorithms/AES.h
@@ -9,19 +9,14 @@
 #define SUCCESS 0
 #define FAILURE !SUCCESS
 
-<<<<<<< HEAD
 int encrypt(const unsigned char* pwd, EVP_CIPHER* cipher, const unsigned char* in, int lenIn, unsigned char** out, int* lenOut);
 
 int decrypt(const unsigned char* pwd, EVP_CIPHER* cipher, const unsigned char* in, int lenIn, unsigned char** out, int* lenOut);
 
-int saveEncryptedData(unsigned char *out, int len,unsigned char *where);
+int saveEncryptedData(unsigned char *out, int len, unsigned char *where);
 
-int obtenerEntrada(unsigned char *in, unsigned char *inB64);
+int loadEncryptedData(unsigned char *out, int len, unsigned char *where);
 
-=======
-int encrypt(const unsigned char* key, EVP_CIPHER* cipher, const unsigned char* in, unsigned char** out, int* len);
+//int obtenerEntrada(unsigned char *in, unsigned char *inB64);
 
-int saveEncryptedData(unsigned char *out, int len,unsigned char *where);
-
->>>>>>> cf1be73625eee1acc703fe98887e48788cebe3f2
-void mostrarKey(unsigned char key[]);
+//void mostrarKey(unsigned char key[]);
